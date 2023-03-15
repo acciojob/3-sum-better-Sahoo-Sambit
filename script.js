e316c68242379772f559d924f0156edd48777aab
Sambit_Sahoo <93509732+Sahoo-Sambit@users.noreply.github.com>
2023-03-15 08:44:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  arr.sort((a,b)=>a-b);
+  let closestSum= Infinity;
+  for (let i = 0; i < arr.length-2; i++) {
+  	let left=i+1,right=arr.length-1;
+	while (left<right) {
+		const currSum=arr[i]+arr[left]+arr[right]
+	}
+	  if(Math.abs(currSum-target)<Math.abs(closestSum-target)){
+		  closestSum=currSum;
+	  }
+	  if(currSum<target){
+		  left++;
+	  }else{
+		  right--;
+	  }
+  }
 }
 
 module.exports = threeSum;
